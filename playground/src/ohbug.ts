c651b623de0a92d87107ca4f770740ca6c1909e6
chenyueban <jasonchan0527@gmail.com>
2022-08-03 15:55:16 +0800

    chore: update deps

--- playground/src/ohbug.ts
+++ playground/src/ohbug.ts
@@ -3,7 +3,7 @@ import { OhbugErrorBoundary } from '@ohbug/react'
 import OhbugExtensionFeedback from '../..'
 
 export const client = Ohbug.setup({
-  apiKey: '5c00f9696c97880bed9dc701168fa734e5f063132ba5a9df6775678b67b91db8',
+  apiKey: 'eae0790e73c5e501db86f88a05d31775c6b9a9d70cda230fef2a15ac66ace71e',
   appType: 'react',
   appVersion: __APP_VERSION__,
 })
